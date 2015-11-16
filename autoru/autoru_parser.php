261ffa83a9141809d8ae3ec853c77ecb34e12dab
thebender <thebendertt@gmail.com>
2015-11-16 21:02:03 +0300

    11

---
+++ autoru/autoru_parser.php
@@ -0,0 +1,27 @@
+<?php
+
+    class autoru_offer
+    {
+
+        public $id;
+        public $offer_create_time;
+        public $picture_url;
+        public $mark;
+        public $model_gen;
+        public $price;
+        public $manufacture_year;
+        public $mileage;
+        public $color;
+        public $body_type;
+        public $owner_phone;
+        public $city;
+
+        public function __construct($sales_list_item)
+        {
+
+        }
+
+
+    }
+
+?>
---
+++ parser.php
@@ -0,0 +1,23 @@
+<?php
+
+include_once '/home/thebender/PhpstormProjects/carparser/simple_html_dom.php';
+
+$page = file_get_html('http://auto.ru/cars/audi/all/');
+
+foreach($page->find('div[class="sales-list-item"]') as $element) {
+    if (isset($element->find('tr[class="sales-list-row in-logging"]')[0]->{'data-sale_id'})) {
+        echo 'ID: ' . $element->find('tr[class="sales-list-row in-logging"]')[0]->{'data-sale_id'} . '</br>';
+        echo 'offer_create_time ' . '</br>';
+        echo 'picture_url ' . $element->find('img')[0]->src . '</br>';
+        echo 'mark'  . '</br>';
+        echo 'model ' . 'generation' . '</br>';
+        echo 'price ' . '</br>';
+        echo 'manufacture_year ' . '</br>';
+        echo 'mileage ' . '</br>';
+        echo 'color ' . '</br>';
+        echo 'body_type ' . '</br>';
+        echo 'owner_phone ' . '</br>';
+        echo 'city ' . '</br>';
+    }
+}
+?>
